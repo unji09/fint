85f233cf2c67362bd0c6df8efd1459c98c34d210
정은지 <unji0907@naver.com>
2026-05-05 13:55:59 +0900

    feat : 활동 삭제 api 추가

--- backend/src/main/java/com/ssafy/fint/domain/activity/controller/ActivityController.java
+++ backend/src/main/java/com/ssafy/fint/domain/activity/controller/ActivityController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,12 @@ public class ActivityController implements ActivitySwagger {
             @RequestParam(required = false) Long dealId
     ) {
         return ApiResponse.ok(activityService.findDetail(activityId, dealId));
+    }
+
+    @Override
+    @DeleteMapping("/{activityId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long activityId) {
+        activityService.delete(activityId);
     }
 }
