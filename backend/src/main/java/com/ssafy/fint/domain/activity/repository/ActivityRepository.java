0dec7d87349c902070b921e90af3dcd2c5fb14c2
정은지 <unji0907@naver.com>
2026-05-06 13:14:59 +0900

    refactor : Activity 단건 조회에 EntityGraph 적용

--- backend/src/main/java/com/ssafy/fint/domain/activity/repository/ActivityRepository.java
+++ backend/src/main/java/com/ssafy/fint/domain/activity/repository/ActivityRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.fint.domain.activity.repository;
 
 import com.ssafy.fint.domain.activity.entity.Activity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,6 +9,7 @@ import java.util.Optional;
 public interface ActivityRepository
         extends JpaRepository<Activity, Long>, ActivityRepositoryCustom {
 
+    @EntityGraph(attributePaths = {"deal", "deal.account", "pipelineStage"})
     Optional<Activity> findByActivityIdAndUser_UserIdAndUser_Tenant_TenantId(
             Long activityId, Long userId, Long tenantId);
 }
