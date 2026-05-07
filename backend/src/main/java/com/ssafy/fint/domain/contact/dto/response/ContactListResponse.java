894d614cb92955c27b90a52dce40945bf4557fc5
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-05-07 11:43:43 +0900

    feat : ContactListResponse DTO 추가

---
+++ backend/src/main/java/com/ssafy/fint/domain/contact/dto/response/ContactListResponse.java
@@ -0,0 +1,28 @@
+package com.ssafy.fint.domain.contact.dto.response;
+
+import com.ssafy.fint.domain.contact.entity.Contact;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ContactListResponse {
+
+    private Long contactId;
+    private String name;
+    private String title;
+    private String phone;
+    private String email;
+    private String personality;
+
+    public static ContactListResponse from(Contact contact) {
+        return ContactListResponse.builder()
+            .contactId(contact.getContactId())
+            .name(contact.getName())
+            .title(contact.getTitle())
+            .phone(contact.getPhone())
+            .email(contact.getEmail())
+            .personality(contact.getPersonality())
+            .build();
+    }
+}
