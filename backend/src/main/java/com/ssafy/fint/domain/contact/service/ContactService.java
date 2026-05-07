13318539fcb24384f7e282d24693541a56fdb4f2
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-05-07 10:59:14 +0900

    feat : ContactService 추가

---
+++ backend/src/main/java/com/ssafy/fint/domain/contact/service/ContactService.java
@@ -0,0 +1,31 @@
+package com.ssafy.fint.domain.contact.service;
+
+import com.ssafy.fint.domain.contact.dto.request.ContactCreateRequest;
+import com.ssafy.fint.domain.contact.dto.response.ContactCreateResponse;
+import com.ssafy.fint.domain.contact.entity.Contact;
+import com.ssafy.fint.domain.contact.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ContactService {
+
+    private final ContactRepository contactRepository;
+
+    @Transactional
+    public ContactCreateResponse createContact(ContactCreateRequest request) {
+        Contact contact = Contact.builder()
+            .accountId(request.getAccountId())
+            .name(request.getName())
+            .title(request.getTitle())
+            .phone(request.getPhone())
+            .email(request.getEmail())
+            .personality(request.getPersonality())
+            .build();
+
+        return ContactCreateResponse.from(contactRepository.save(contact));
+    }
+}
