7de1acad3c3b640badac84bc63588b06435d8018
dnfdjssl88 <dnfdjssl88@naver.com>
2026-05-07 15:06:19 +0900

    fix : delete로 변경

--- backend/src/main/java/com/ssafy/fint/domain/deal/controller/DealController.java
+++ backend/src/main/java/com/ssafy/fint/domain/deal/controller/DealController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,7 +59,7 @@ public class DealController implements DealSwagger {
     }
 
     @Override
-    @PatchMapping("/{dealId}/delete")
+    @DeleteMapping("/{dealId}")
     public ApiResponse<Void> softDelete(
             @AuthenticationPrincipal CustomUserDetails me,
             @PathVariable Long dealId
