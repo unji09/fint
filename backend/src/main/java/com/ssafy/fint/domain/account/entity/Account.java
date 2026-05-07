bc8f792750d2664f5ad09036b014caee869e684a
ppg0226 <ppg0226@naver.com>
2026-05-08 05:57:38 +0900

    feat : Account 도메인 엔티티 재설계 (user 필드 제거, Mood enum, AccountUserAssignment 신규)

--- backend/src/main/java/com/ssafy/fint/domain/account/entity/Account.java
+++ backend/src/main/java/com/ssafy/fint/domain/account/entity/Account.java
@@ -1,15 +1,11 @@
 package com.ssafy.fint.domain.account.entity;
 
-import com.ssafy.fint.domain.user.entity.User;
 import com.ssafy.fint.global.common.entity.BaseSoftDeletableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,7 +14,7 @@ import lombok.NoArgsConstructor;
 
 /**
  * 고객사(Account).
- * 영업 대상 회사 단위. 담당 사원(User)이 owner.
+ * 영업 대상 회사 단위. 책임자(사원) 정보는 account_user_assignment 테이블로 분리됨.
  */
 @Entity
 @Table(name = "accounts")
@@ -31,10 +27,6 @@ public class Account extends BaseSoftDeletableEntity {
     @Column(name = "account_id")
     private Long accountId;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
     @Column(name = "name", nullable = false, length = 200)
     private String name;
 
@@ -45,8 +37,7 @@ public class Account extends BaseSoftDeletableEntity {
     private String bizNo;
 
     @Builder
-    private Account(User user, String name, String industry, String bizNo) {
-        this.user = user;
+    private Account(String name, String industry, String bizNo) {
         this.name = name;
         this.industry = industry;
         this.bizNo = bizNo;
@@ -62,9 +53,5 @@ public class Account extends BaseSoftDeletableEntity {
 
     public void changeBizNo(String bizNo) {
         this.bizNo = bizNo;
-    }
-
-    public void changeOwner(User user) {
-        this.user = user;
     }
 }
