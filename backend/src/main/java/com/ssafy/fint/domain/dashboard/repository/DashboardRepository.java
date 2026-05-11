43d9ca74e367e886c5abfd8d2e235273e6fbc665
ppg0226 <ppg0226@naver.com>
2026-05-11 10:12:16 +0900

    feat : 대시보드 목록 조회 API 구현 (GET /dashboards)

--- backend/src/main/java/com/ssafy/fint/domain/dashboard/repository/DashboardRepository.java
+++ backend/src/main/java/com/ssafy/fint/domain/dashboard/repository/DashboardRepository.java
@@ -1,7 +1,12 @@
 package com.ssafy.fint.domain.dashboard.repository;
 
 import com.ssafy.fint.domain.dashboard.entity.Dashboard;
+import com.ssafy.fint.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DashboardRepository extends JpaRepository<Dashboard, Long> {
+
+    List<Dashboard> findTop20ByOwnerOrderByLastAccessedAtDesc(User owner);
 }
