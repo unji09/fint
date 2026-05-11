f3e879673b4a64180e999763902de512be2b4df3
ppg0226 <ppg0226@naver.com>
2026-05-11 10:57:18 +0900

    feat : 대시보드 상세 조회 API 구현

--- backend/src/main/java/com/ssafy/fint/domain/dashboard/repository/DashboardWidgetRepository.java
+++ backend/src/main/java/com/ssafy/fint/domain/dashboard/repository/DashboardWidgetRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.fint.domain.dashboard.repository;
 
+import com.ssafy.fint.domain.dashboard.entity.Dashboard;
 import com.ssafy.fint.domain.dashboard.entity.DashboardWidget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DashboardWidgetRepository extends JpaRepository<DashboardWidget, Long> {
 
     Optional<DashboardWidget> findByDashboardWidgetIdAndDashboard_DashboardId(Long widgetId, Long dashboardId);
+
+    List<DashboardWidget> findByDashboard(Dashboard dashboard);
 }
